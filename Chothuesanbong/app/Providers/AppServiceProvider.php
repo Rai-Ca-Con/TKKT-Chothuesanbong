d912dc4174b4501b08966bce7c2d27418c9f7a02
nguyenanhtu <nguyenanhtu2172gmail.com>
2025-04-01 17:19:22 +0700

    Khoi tao du an

---
+++ Chothuesanbong/app/Providers/AppServiceProvider.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Providers;
+
+use Illuminate\Support\ServiceProvider;
+use App\Repositories\UserRepository;
+use App\Services\UserService;
+
+class AppServiceProvider extends ServiceProvider
+{
+    /**
+     * Register any application services.
+     */
+    public function register(): void
+    {
+        
+        $this->app->singleton(UserRepository::class, function ($app) {
+            return new UserRepository();
+        });
+        
+        $this->app->singleton(UserService::class, function ($app) {
+            return new UserService($app->make(UserRepository::class));
+        });
+    }
+
+    /**
+     * Bootstrap any application services.
+     */
+    public function boot(): void
+    {
+        //
+    }
+}
---
+++ Chothuesanbong/app/Repositories/UserRepository.php
@@ -0,0 +1,22 @@
+<?php
+namespace App\Repositories;
+
+use App\Models\User;
+
+class UserRepository
+{
+    public function getAll()
+    {
+        return User::all();
+    }
+
+    public function findById($id)
+    {
+        return User::findOrFail($id);
+    }
+
+    public function create(array $data)
+    {
+        return User::create($data);
+    }
+}
