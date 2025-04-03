699141b620cf9a38650c120053845e1fbefa7e59
nguyenanhtu <nguyenanhtu2172gmail.com>
2025-04-03 10:30:00 +0700

    tao bang user

--- Chothuesanbong/database/migrations/2014_10_12_000000_create_users_table.php
+++ Chothuesanbong/database/migrations/2014_10_12_000000_create_users_table.php
@@ -1,3 +1,4 @@
+
 <?php
 
 use Illuminate\Database\Migrations\Migration;
@@ -12,13 +13,21 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('users', function (Blueprint $table) {
-            $table->id();
+            $table->uuid('id')->primary();
             $table->string('name');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->string('phone_number')->default('');
+            $table->string('address')->default('');
+            $table->integer('status')->default('0');
+            $table->boolean('is_admin')->default(false);
+            $table->string('refresh_token')->default('');
+            $table->string('google_id')->nullable();
+            $table->text('avatar')->nullable();;         // Đường dẫn hình ảnh
             $table->rememberToken();
             $table->timestamps();
+            $table->softDeletes();
         });
     }
 
